a2de1592376dc9b336ee810e5264286d8f908877
kgm20063 <kgm20063@gmail.com>
2024-01-12 05:31:33 +0900

    FEAT: 이미지 경로 추가

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as S from './styled';
 import logo from '../../assets/logo.png'
+import profile from '../../assets/프로필.png'
 import { pullLogin } from '../../utils/login';
 import { clearIdx } from '../../utils/idx';
 
@@ -10,11 +11,11 @@ function Header() {
             <S.ContentsDiv>
                 <Link to="/" onClick={clearIdx()}>
                     <S.LogoBox>
-                        <img src={logo} alt="" />
+                        <img src={logo} alt="logo" />
                     </S.LogoBox>
                 </Link>
                 <Link to="/login">
-                    {pullLogin() ? <S.UserBox>{pullLogin()}</S.UserBox> : <S.LoginBox></S.LoginBox>}
+                    {pullLogin() ? <S.UserBox><img src={profile} alt="user" />{pullLogin()}</S.UserBox> : <S.LoginBox><img src={profile} alt="logIn" /></S.LoginBox>}
                 </Link>
             </S.ContentsDiv>
         </S.HeaderDiv>
